326ab62b1c6abd3131d828bd1d7abd9815cd7262
Pareekshith A Abhimanyu Raja <paa36@afsconnect2.njit.edu>
2017-10-22 08:51:23 -0400

    Added index

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+//turn on debugging messages
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+//Class to load classes it finds the file when the progrm starts to fail for calling a missing class
+class Manage {
+    public static function autoload($class) {
+        //you can put any file name or directory here
+        include $class . '.php';
+    }
+}
+
+spl_autoload_register(array('Manage', 'autoload'));
+
+$obj = new main();
+
+
+class main {
+
+    public function __construct()
+    {
+
+        //set default page request when no parameters are in URL
+        $pageRequest = 'homepage';
+        //check if there are parameters
+        if(isset($_REQUEST['page'])) {
+            //load the type of page the request wants into page request
+            $pageRequest = $_REQUEST['page'];
+        }
+        //instantiate the class that is being requested
+        $page = new $pageRequest;
+
+
+        if($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $page->get();
+        } else {
+            $page->post();
+        }
+
+    }
+
+}
